635fed8dc4f0ba9658a8a3464d2b5a20e697fad0
ibianche <inna.pomorska@gmail.com>
2021-08-17 18:40:56 +0300

    Repair of the first if/else

--- js/script.js
+++ js/script.js
@@ -2,18 +2,19 @@ let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
 
+
 let computerMove = 'nieznany ruch';
 
-if (playerInput == '1') {
-  playerMove = 'kamień';
+if (randomNumber == '1') {
+  computerMove = 'kamień';
 }
 
-else if (playerInput == '2') {
-  playerMove = 'nożyce';
+else if (randomNumber == '2') {
+  computerMove = 'nożyce';
 }
 
-else if(playerInput == '3') {
-  playerMove = 'papier';
+else if (randomNumber == '3') {
+  computerMove = 'papier';
 }
 
 printMessage('Mój ruch to: ' + computerMove);
